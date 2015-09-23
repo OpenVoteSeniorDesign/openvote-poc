8d8e048de355851b8c46271ec311253a5ad21222
William Mills <millsw@utexas.edu>
2015-09-23 14:48:52 -0500

    Overwrote with the eclipse hello world project

---
+++ OpenVote/src/com/openvote/OpenVoteServlet.java
@@ -0,0 +1,13 @@
+package com.openvote;
+
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class OpenVoteServlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hello, world");
+	}
+}
