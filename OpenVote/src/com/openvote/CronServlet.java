fa058381117c0cd1db0c58aaf5f88b81718e84f2
Dylan Zika <dylanzika@gmail.com>
2015-10-26 14:36:19 -0500

    Mostly Done with Cron Vote Servlet. The only thing under question is how fake vote batches will be sent, from vote ingestion. Currently they are sent through ofy through an Integer.class this should be changed possibly through a FakeVote.class wrapper around a simple interger.
    Signed-off-by: Dylan Zika <dylanzika@gmail.com>

--- OpenVote/src/com/openvote/CronServlet.java
+++ OpenVote/src/com/openvote/CronServlet.java
@@ -27,7 +27,6 @@ public class CronServlet extends HttpServlet {
     private static final int MAX_TIME_OUT = 100;
     private static final int TEMP_VOTE_BUFFER_FILL = Candidate.values().length + 1;
 
-
     // This adds a random amount of latency with a specified range in values and a desired offset in
     // units of seconds
     public void randomSleepDelay(){
@@ -41,80 +40,99 @@ public class CronServlet extends HttpServlet {
 
     @Override
     public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException{
+
+        /* 
+         * 1. 
+         * 
+         * Random Time
+         */
         
-        // 1 Random Time
         randomSleepDelay();
 
-        // 2 Get votes from data store filtered on property published
-        List<Vote> voteList = ObjectifyService.ofy().load().type(Vote.class).filter("published ==", false).list(); 
+        /* 
+         * 2. 
+         * 
+         * Get votes from data store filtered on property published
+         */
         
-        // 3 Taly Up all of the unb-pulished votes                                                                                                                // vote
+        List<Vote> voteList = ObjectifyService.ofy().load().type(Vote.class).filter("published ==", false).list();
+
+        /* 
+         * 3. 
+         * 
+         * Taly Up all of the unb-pulished votes // vote
+         */
+        
         Map<Candidate, Integer> tally = new HashMap<Candidate, Integer>();
         for(Vote v : voteList){
             Candidate key = Candidate.values()[v.getCandidate()];
-            
-            if(tally.containsKey(key)==false){
-                tally.put(key,1);
+
+            if(tally.containsKey(key) == false){
+                tally.put(key, 1);
             }else{
-                tally.put(key,tally.get(key)+1);
+                tally.put(key, tally.get(key) + 1);
             }
         }
+
+        /*
+         * 4. 
+         * 
+         * Anonymity Check :
+         * Require at least one vote per candidate must be present before we can move votes over
+         * numFakeBatches saved by CastFakeVotes atomically and assumed to be only first Integer in
+         * data store
+         * Alternative condition:
+         * boolean anonimityTransfer = (tally.keySet().size() > Candidate.values().length) &&
+         * (candList.size()>TEMP_VOTE_BUFFER_FILL);
+         */
+
+        int numFakeBatches = ObjectifyService.ofy().load().type(Integer.class).first().getValue(); // this
+                                                                                                   // will
+                                                                                                   // be
+                                                                                                   // number
+                                                                                                   // of
+                                                                                                   // the
+                                                                                                   // fake
+                                                                                                   // vote
+                                                                                                   // clusters
+        int numCandiates = Candidate.values().length;
+        int realVotes = voteList.size() - numFakeBatches * numCandiates;
+
+        // As long as all the talys are greater than realVotes we can go ahead and publish
+        boolean doNotPublish = true;
+        for(Candidate c : tally.keySet()){
+            doNotPublish &= tally.get(c) >= realVotes;
+        }
+
+        /*
+         * 5.
+         * 
+         * Time Out Transfer and Publish Votes Publicly :
+         * Publish to data store after changing the variable
+         * Add Time Out based on MAX_TIME_OUT
+         */
+
+        // Check on Time out
+        TimeOut time = ObjectifyService.ofy().load().type(TimeOut.class).first().getValue();
+        if(time.incrementTimeOut() == true && doNotPublish==true){
+            doNotPublish = false;
+        }
+        ObjectifyService.ofy().save().entity(time).now();
         
-       /* 4 Anonymity Check : 
-        * Require at least one vote per candidate must be present before we can move votes over
-        * numFakeBatches saved by CastFakeVotes atomically and assumed to be only first Integer in data store
-        * 
-        * Alternative condition:
-        * boolean anonimityTransfer = (tally.keySet().size() > Candidate.values().length) && (candList.size()>TEMP_VOTE_BUFFER_FILL);
-        */
-        
-       int numFakeBatches = ObjectifyService.ofy().load().type(Integer.class).first().getValue() ; //this will be number of the fake vote clusters
-       int numCandiates = Candidate.values().length;
-       int realVotes =  voteList.size() - numFakeBatches * numCandiates;
-       
-       //As long as all the talys are greater than realVotes we can go ahead and publish
-       boolean doNotPublish = true ; 
-       for(Candidate c : tally.keySet()){
-           doNotPublish &= tally.get(c) >= realVotes ;
-       }
-        
-       
-        
-       /* TODO 5 Publish or Time Out Check : 
-        * Publish to data store after changing the varible
-        * TODO Add Time Out based on MAX_TIME_OUT
-        * */
-        
-        if(doNotPublish == false){ 
-            for(Vote v: voteList){
-                //1 is going to work
+        // Publish if were good
+        if(doNotPublish == false){
+            for(Vote v : voteList){
+                // 1 is going to work
                 v.publish();
-                ObjectifyService.ofy().save().entities(v);
-                
-                /*2
-                ObjectifyService.ofy().toEntity(c).setProperty("published", true);*/
-            }
-        }else{ 
-            /*
-             * Integer intLoad = ObjectifyService.ofy().load().entity(Integer.class).safe();
-            Integer timeOut = intLoad;
-            
-            if(timeOut.intValue()<MAX_TIME_OUT){ //Other Wise we gotta wait: increment time out counter and do nothing
-                timeOut++;
-            }else{ //If we are here we have reached the time out and we must
-                                          //time out reached do transfer any ways
-                timeout=0;                //reset timeout varible in data store
+                ObjectifyService.ofy().save().entity(v).now();
 
+
+                /*
+                 * 2 I think this may be faster not incuring so much over head of loading and re storing
+                 * ObjectifyService.ofy().toEntity(v).setProperty("published", true);
+                 */
             }
-            
-            ObjectifyService.ofy().save().entity(timeOut);*/
         }
-
-        /* TODO 6 Update Published Taly  
-         * Load TallyClass from data store
-         * Global Tally += local Tally Data of real and vake votes
-         * Save TallClass in data store
-         * */
 
     }
 
