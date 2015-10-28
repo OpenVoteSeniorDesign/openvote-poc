d8d5d71082d569c11e52a56986d73f615e28719b
Reed Cozart <rcozart@utexas.edu>
2015-10-28 14:31:21 -0500

    updates

--- OpenVote/src/com/openvote/SingleResultServlet.java
+++ OpenVote/src/com/openvote/SingleResultServlet.java
@@ -1,3 +1,20 @@
+package com.openvote;
+
+
+import java.io.IOException;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+import java.util.TimeZone;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
 import javax.servlet.http.*;
 
 import com.googlecode.objectify.ObjectifyService;
@@ -9,6 +26,7 @@ public class SingleResultServlet extends HttpServlet {
     public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException{
         //get election from glcoud
     	//List<Vote> voteList = ObjectifyService.ofy().load().type(Vote.class).filter("published ==", true).list();
+    	
     }
 
     @Override
--- OpenVote/war/viewSingleVote.jsp
+++ OpenVote/war/viewSingleVote.jsp
@@ -1,9 +1,27 @@
+<%@ page import="com.openvote.Vote" %>
+<%@ page import="com.googlecode.objectify.Key" %>
+<%@ page import="com.googlecode.objectify.ObjectifyService" %>
+<%@ page import="com.openvote.Candidate" %>
+<%@ taglib prefix="fn" uri="http://java.sun.com/jsp/jstl/functions" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ page import="com.googlecode.objectify.*" %>
+<%@ page import="java.util.*" %>
+
 <html>
 
 	<head>
    		<link type="text/css" rel="stylesheet" href="/stylesheets/bootstrap.css" />
  	</head>
 
+ 	<%
+ 		Long id = (Long) request.getAttribute("id");
+ 		Vote v = ObjectifyService.ofy()
+ 				.load.type(Vote.class)
+ 			    .filterKey("=", id);
+ 			     
+	%>
+ 	
 	<body>
     	<div class="container">
   			<h2>OpenVote</h2>
